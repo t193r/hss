a1be414dcd5c0e7ec4dc03725efaa9d184347a5c
ikhwan <ikhwanaly@ymail.com>
2013-05-25 22:53:20 +0700

    Added my work

---
+++ user/ikhwan/TODO/dynamic-variable-with-condition./dynamic-variable-with-conditon.sh
@@ -0,0 +1,15 @@
+packet=`dpkg --get-selections | wc -l`;
+# echo $packet
+name=`whoami`
+if [[ $packet = 2491 ]]; then
+	echo "Hi $name Jumlah packet anda : $packet "
+	echo ""
+	echo "Packet ter-install sangat banyak " 
+	echo "Silahakan buang packet-packet yang tidak perlu"
+	echo ""
+fi
+
+if [[ $packet -gt 1000 ]]; then
+	echo "jumlah packet anda ( $packet ) lebih dari 1000 :D "
+	
+fi
