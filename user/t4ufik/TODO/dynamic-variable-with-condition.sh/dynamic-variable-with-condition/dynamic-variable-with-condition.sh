407c4f11b414ae4c59aa4c344738692bfa2ad8b2
Taufik Ismail <smail_gokiel@yahoo.com>
2013-05-25 21:47:48 +0000

    tugas

---
+++ user/t4ufik/TODO/dynamic-variable-with-condition.sh/dynamic-variable-with-condition/dynamic-variable-with-condition.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+jumlahpaket=$(dpkg -l | grep ii | wc -l
+)
+
+echo $jumlahpaket;
+
+if [[ $jumlahpaket -gt 1000 ]]; then
+	echo "jumlah paket yang ter-install sangat banyak"
+	echo "disarankan untuk membuang paket tidak perlu"
+fi
