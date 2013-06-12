4eaff6e07a816ddb4d10333d8daa05f53c238445
a903s <49035root@gmail.com>
2013-06-12 11:05:29 +0700

    How to make script calculator

---
+++ user/a903s/Test-1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# argumen1
+echo $1 adalah salah satu $2 popular di $3
+
+# argumen2
+clear
+echo "Nama script anda : $0";
+echo "Banyaknya argumen : $#";
+echo "Argumennya adalah : $*";
